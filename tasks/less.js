e15914d0bab31a768cbd4b06b57f17d2e283a6a9
Andy Hill <athill@iu.edu>
2015-07-23 15:39:05 -0400

    add minify/sourcemaps to less processing

--- tasks/less.js
+++ tasks/less.js
@@ -1,19 +1,19 @@
 var gulp = require('gulp'),
-	less = require('gulp-less'),
-    // $ = require('gulp-load-plugins')(),
+	// less = require('gulp-less'),
+	csswring = require('csswring'),
+	// postcss = require('gulp-postcss'),
+    $ = require('gulp-load-plugins')(),
     utils = require('../utils'),
     config = global.config.less;
 
 gulp.task('less', ['clean-less'], function() {
     var src = utils.findSourceDirectories(config.entryPoint, config.srcdir);
-
     return gulp.src(src, { base: process.cwd() })
-        // .pipe(utils.browserify)
-        .pipe(less())
         .pipe(utils.moduleAwareRename(config.srcdir, config.outputName))
-        // .pipe($.sourcemaps.init({loadMaps: true})) // loads map from browserify file;
-        // .pipe($.uglify({ mangle: false }))
-        // .pipe($.sourcemaps.write('./')) // writes .map file
+        .pipe($.less())
+        .pipe($.sourcemaps.init()) 
+        .pipe($.if('**/*.css', $.postcss([csswring])))
+        .pipe($.sourcemaps.write('./')) // writes .map file
         .pipe(gulp.dest(config.dest));
 });   
 
