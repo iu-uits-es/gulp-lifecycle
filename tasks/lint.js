fed034a35089a1fc61d35bf046a101be0ee3e917
James Bennett <jawbenne@iu.edu>
2015-07-23 17:21:25 -0400

    Cleaned up some code in the linting task and added React linting dependency

--- tasks/lint.js
+++ tasks/lint.js
@@ -3,7 +3,6 @@ var gulp = require('gulp'),
     utils = require('../utils'),
     config = global.config;
 
-
 // Send a notification when JSRC fails,
 // so that you know your changes didn't build
 function jscsNotify(file) {
@@ -28,20 +27,7 @@ var types = ['scripts', 'spec', 'it'];
 for (var i = 0; i < types.length; i++) {
 	var type = types[i];
 	var src = utils.findSourceDirectories(config[type].entryPoint, config[type].srcdir);
-	console.log(src);
-	createLintTask('lint-'+type, src);	
+	createLintTask('lint-' + type, src);
 }
 
-
-// Lint our source code
-// var src = utils.findSourceDirectories(config.scripts.entryPoint, config.srcdir);
-// createLintTask('lint-src', src);
-
-// Lint our test code
-//createLintTask('lint-spec', [config.spec.src]);
-
-// Lint our test code
-//createLintTask('lint-it', [config.it.src]);
-
-//gulp.task('lint', ['lint-src', 'lint-spec', 'lint-it']);
-gulp.task('lint', ['lint-src']);
+gulp.task('lint', ['lint-src', 'lint-spec', 'lint-it']);
