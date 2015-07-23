d53c165eb6ff94af62f0d0ce80a4b12c87f70ae6
James Bennett <jawbenne@iu.edu>
2015-07-23 10:25:36 -0400

    Fixed issue with loading of config.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 
 module.exports = function(gulp, config){
-    var config = Object.assign(require('config.js'), config);
+    var config = Object.assign(require('./config.js'), config);
 
     require('require-dir')('./tasks', {
         recurse: true
