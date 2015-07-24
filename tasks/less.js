52b3b17b68046f9a30b7d2ec15d1f5acb7f63a88
James Bennett <jawbenne@iu.edu>
2015-07-24 11:47:29 -0400

    Modified the base directory when less is executed so LESS references will make more sense

--- tasks/less.js
+++ tasks/less.js
@@ -8,10 +8,9 @@ gulp.task('less', ['clean-less'], function() {
     var src = utils.findSourceDirectories(config.entryPoint, config.srcdir);
     return gulp.src(src, { base: process.cwd() })
         .pipe(utils.moduleAwareRename(config.srcdir, config.outputName))
-        .pipe($.less())
-        .pipe($.sourcemaps.init()) 
+        .pipe($.less(), { base: process.cwd() })
+        .pipe($.sourcemaps.init())
         .pipe($.if('**/*.css', $.postcss([csswring])))
         .pipe($.sourcemaps.write('./')) // writes .map file
         .pipe(gulp.dest(config.dest));
-});   
-
+});
