2ca0313b60b4e0f8b3d6ddc2b8f5a388468afa52
James Bennett <jawbenne@iu.edu>
2015-12-18 15:16:49 -0500

    SIT-271: Added ES2016 support

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -6,6 +6,7 @@ var $ = require('gulp-load-plugins')(),
     rename = require('gulp-rename'),
     es2015 = require('babel-preset-es2015'),
     react = require('babel-preset-react'),
+    stage0 = require('babel-preset-stage-0'),
     through2 = require('through2'),
     utils = require('../utils'),
     config = global.config.scripts;
@@ -17,7 +18,7 @@ module.exports = function(gulp) {
         return gulp.src(src, { base: process.cwd() })
             .pipe(through2.obj(function(file, enc, next) {
                 browserify(file.path)
-                  .transform(babelify.configure({ignore: 'node_modules', presets: [es2015, react]}))
+                    .transform(babelify.configure({ignore: 'node_modules', presets: [es2015, react, stage0]}))
                   .bundle(function (err, result) {
                       file.contents = result;
                       next(null, file);
