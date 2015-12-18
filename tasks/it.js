f3348e816de45a7df142f4847ebad045fdf98a65
James Bennett <jawbenne@iu.edu>
2015-12-18 13:39:31 -0500

    SIT-271: Fixed two issues with the lifecycle
    * Fixed issue where we were reusing a stale filesystem handle which was
    closed and would cause issues during a gulp watch
    * Fixed issue with clean tasks which were not doing the operation in a
    synchronous way and caused dependent tasks to not be executed in newer
    version of gulp

--- tasks/it.js
+++ tasks/it.js
@@ -7,7 +7,14 @@ module.exports = function(gulp) {
         var src = utils.findSourceDirectories(config.entryPoint, config.srcdir);
 
         return gulp.src(src, { base: process.cwd() })
-            .pipe(utils.browserify)
+            .pipe(through2.obj(function(file, enc, next) {
+                browserify(file.path)
+                  .transform(babelify.configure({ignore: 'node_modules', presets: [es2015, react]}))
+                  .bundle(function (err, result) {
+                      file.contents = result;
+                      next(null, file);
+                  })
+            }))
             .pipe(utils.moduleAwareRename(config.srcdir, config.outputName))
             .pipe(gulp.dest(config.dest))
             .pipe(jasmine(config.jasmineConfig));
