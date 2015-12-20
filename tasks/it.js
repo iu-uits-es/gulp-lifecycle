2489063eb8f73e0f2f50b43b541856d3eaf8ddfb
James Bennett <jawbenne@iu.edu>
2015-12-20 00:08:49 -0500

    Added missing stage0 support for tests and ITs

--- tasks/it.js
+++ tasks/it.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
   babelify = require('babelify'),
   es2015 = require('babel-preset-es2015'),
   react = require('babel-preset-react'),
+  stage0 = require('babel-preset-stage-0'),
   through2 = require('through2'),
   utils = require('../utils'),
   config = global.config.spec;
@@ -15,7 +16,7 @@ module.exports = function(gulp) {
         return gulp.src(src, { base: process.cwd() })
             .pipe(through2.obj(function(file, enc, next) {
                 browserify(file.path)
-                  .transform(babelify.configure({ignore: 'node_modules', presets: [es2015, react]}))
+                  .transform(babelify.configure({ignore: 'node_modules', presets: [es2015, react, stage0]}))
                   .bundle(function (err, result) {
                       if(err) {
                           console.error(err.message);
